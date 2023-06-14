29d105e543a1fb46a59f0c390fc430acc20dd505
SheedGuy <osheed420@gmail.com>
2023-06-13 20:46:52 -0400

    Initial SMM Panel Code
    - Created functions for checking services, adding likes, and checking order status
    - Created a file to contain my keys as not to expose them online!

---
+++ smmServices.py
@@ -0,0 +1,29 @@
+# This file contains several functions for interacting with the SMM panel API
+import requests
+import keys
+
+secret = keys.PeakerrKey
+
+# Getting all the services
+def GetServices():
+    resp = requests.post('https://peakerr.com/api/v2?key=' + secret + '&action=services')
+    return resp.json()
+
+# Placing a new order for likes
+def AddLikes(link, quantity):
+    postLink = link
+    resp = requests.post("https://peakerr.com/api/v2?key=" + secret 
+                         + "&action=add&service=14097&link=" + postLink 
+                         + "&quantity=" + quantity)
+    return resp.json()
+
+def OrderStatus(orderID):
+    resp = requests.post("https://peakerr.com/api/v2?key=" + secret 
+                         + "&action=status&id=" + str(orderID))
+    return resp.json()
+
+
+# These all function Properly
+# Todo:
+# Add a function to add followers
+# Create a data struct for services
